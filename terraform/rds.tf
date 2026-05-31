b9289a2b1c9631d5660b6eb80bd83707f389cd17
shangchoco <cherrypetal8764@gmail.com>
2026-05-31 12:41:08 +0900

    chore: 보안 강화(환경 변수 분리) 및 불필요한 설정 파일 제거

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,11 +4,11 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro" # 프리티어 범위
-  username             = "admin"
-  password             = "Password123!" # 실제 사용 시에는 AWS Secrets Manager 등을 써야 합니다!
+  username             = var.db_username
+  password             = var.db_password # 실제 사용 시에는 AWS Secrets Manager 등을 써야 합니다!
   skip_final_snapshot  = true
 
-  publicly_accessible = true
+  publicly_accessible = false
 
   # 앞서 만든 보안 그룹과 서브넷 그룹 연결
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
